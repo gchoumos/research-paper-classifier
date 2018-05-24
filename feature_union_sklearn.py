34f6abaa8e5e4949ef23cffa196108cd99522770
George Ch <g.choumos@gmail.com>
2018-05-24 20:13:51 +0300

    Add and modify appropriately the ItemSelector from scikit learn examples

---
+++ feature_union_sklearn.py
@@ -0,0 +1,95 @@
+# Author: Matt Terry <matt.terry@gmail.com>
+#
+# License: BSD 3 clause
+from __future__ import print_function
+
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets.twenty_newsgroups import strip_newsgroup_footer
+from sklearn.datasets.twenty_newsgroups import strip_newsgroup_quoting
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    """For data grouped by feature, select subset of data at a provided key.
+
+    The data is expected to be stored in a 2D data structure, where the first
+    index is over features and the second is over samples.  i.e.
+
+    >> len(data[key]) == n_samples
+
+    Please note that this is the opposite convention to scikit-learn feature
+    matrixes (where the first index corresponds to sample).
+
+    ItemSelector only requires that the collection implement getitem
+    (data[key]).  Examples include: a dict of lists, 2D numpy array, Pandas
+    DataFrame, numpy record array, etc.
+
+    >> data = {'a': [1, 5, 2, 5, 2, 8],
+               'b': [9, 4, 1, 4, 1, 3]}
+    >> ds = ItemSelector(key='a')
+    >> data['a'] == ds.transform(data)
+
+    ItemSelector is not designed to handle data grouped by sample.  (e.g. a
+    list of dicts).  If your data is structured this way, consider a
+    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
+
+    Parameters
+    ----------
+    key : hashable, required
+        The key corresponding to the desired value in a mappable.
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        return data_dict[self.key]
+
+
+class TextStats(BaseEstimator, TransformerMixin):
+    """Extract features from each document for DictVectorizer"""
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, posts):
+        return [{'length': len(text),
+                 'num_sentences': text.count('.')}
+                for text in posts]
+
+
+class AbstractTitleAuthorExtractor(BaseEstimator, TransformerMixin):
+    """Extract the subject & body from a usenet post in a single pass.
+
+    Takes a sequence of strings and produces a dict of sequences.  Keys are
+    `subject` and `body`.
+    """
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, lines):
+        features = np.recarray(shape=(len(lines),),
+                               dtype=[('abstract', object), ('title', object), ('author', object)])
+        for i, line in enumerate(lines):
+            abstract, title, author = line[0], line[1], line[2]
+
+            features['abstract'][i] = abstract if abstract==abstract else ''
+            features['title'][i] = title if title==title else ''
+            features['author'][i] = author if author==author else ''
+
+        print("MYSELF: Features shape is {0}".format(features.shape))
+        print("MYSELF: {0}".format(features[0]))
+        #import pdb
+        #pdb.set_trace()
+        return features
