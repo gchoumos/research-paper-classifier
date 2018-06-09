419a48c62ae2058bac138680d320f565c1bcd443
gchoumos <g.choumos@gmail.com>
2018-06-09 13:40:23 +0300

    Tons of trash thrown away and some minor code beautifications

--- data_preparation.py
+++ data_preparation.py
@@ -5,19 +5,19 @@
           in a lot of documents.
         - The names should be removed... They create too much noise
 Name:
-David
-Lee
-Kim
-Alex
-Michael
-Martin
-Park
-Sergei
-John
-Lu
-Institute
-er
-Yu
+    David
+    Lee
+    Kim
+    Alex
+    Michael
+    Martin
+    Park
+    Sergei
+    John
+    Lu
+    Institute
+    er
+    Yu
 """
 import pandas as pd
 import numpy as np
@@ -30,7 +30,7 @@ p = inflect.engine()
 
 from gensim.parsing.preprocessing import STOPWORDS
 
-def write_to_file(data,filename,numbers=False):
+def write_to_file(data, filename, numbers=False):
     with open(filename,"w") as f:
         if numbers == True:
             for l in data:
@@ -49,7 +49,7 @@ punctuation = '|'.join([re.escape(x) for x in string.punctuation.replace('-','')
 # rm_smw:   Remove small words
 # rm_dg:    Remove words that consist of digits only
 # mdash:    Merge dash including words if the no-dash version exists in the data
-def transform_data(dataset,
+def transform_data(data,
                    stopwords,
                    punct,
                    rm_sw=True,
@@ -61,61 +61,54 @@ def transform_data(dataset,
                    ing_ed=True,
                    auth=False):
     # Convert to Pandas series
-    dataset = pd.Series(d for d in dataset)
+    data = pd.Series(d for d in data)
     # Replace the punctuation with space apart from the dashes 
-    dataset = dataset.str.replace(punct,' ')
+    data = data.str.replace(punct,' ')
     # Split it
-    dataset = dataset.str.split()
+    data = data.str.split()
     # Are there any Nan Values? Replace them
-    dataset = pd.Series([[] if x is np.nan else x for x in dataset])
+    data = pd.Series([[] if x is np.nan else x for x in data])
     # Remove the stopwords
     if rm_sw == True:
-        dataset = pd.Series([[word for word in d if word not in stopwords] for d in dataset])
+        data = pd.Series([[word for word in d if word not in stopwords] for d in data])
     # Remove words with length 1 (many occured after the punctuation removal)
     if rm_smw == True:
-        dataset = pd.Series([[word for word in d if len(word)>1] for d in dataset])
+        data = pd.Series([[word for word in d if len(word)>1] for d in data])
     # Remove digit-only words:
     if rm_dg == True:
-        dataset = pd.Series([[word for word in d if not word.isdigit()] for d in dataset])
+        data = pd.Series([[word for word in d if not word.isdigit()] for d in data])
     # Some special handling for the authors
     # - Remove universit*
     if auth == True:
-        dataset = pd.Series([[word for word in d if not word.lower().startswith('univ')] for d in dataset])
+        data = pd.Series([[word for word in d if not word.lower().startswith('univ')] for d in data])
 
         # Extra stuff for common names
         #common_names = ['david','lee','kim','alex','michael','martin','park','sergei','john','lu','institute','er','yu']
-        #dataset = pd.Series([[word for word in d if word.lower() not in common_names] for d in dataset])
+        #data = pd.Series([[word for word in d if word.lower() not in common_names] for d in data])
     # Merge dash-including words together if the combined version exists
     # This one takes some time so by default it is not performed
     if mdash == True:
         # Get the list of dash-including-words
         dash_words = set()
         non_dashed = set()
-        for line in dataset:
+        for line in data:
             for word in line:
                 if "-" in word and word not in dash_words:
                     dash_words.add(word)
                     non_dashed.add(word.replace('-',''))
 
-        # Check which of them exist without a dash
-        #found_words = set()
-        #for line in dataset:
-        #    for word in line:
-        #        if word in dash_words and word.replace('-','') in non_dashed:
-        #            found_words.add(word)
-
         # Replace those that exist
-        for j, line in enumerate(dataset):
+        for j, line in enumerate(data):
             print("line {0}".format(j))
             for k, word in enumerate(line):
                 if word in dash_words and word.replace('-','') in non_dashed:
-                    dataset[j][k] = word.replace('-','')
+                    data[j][k] = word.replace('-','')
 
     if mplural == True:
         # This will only recognize cases of a final s.
         candidates = set()
         singulars = set()
-        for line in dataset:
+        for line in data:
             for word in line:
                 if len(word) > 4 and word.endswith('s'):
                     candidates.add(word)
@@ -123,47 +116,47 @@ def transform_data(dataset,
                     singulars.add(word)
 
         # Replace the simple plurals with the singulars
-        for k, line in enumerate(dataset):
+        for k, line in enumerate(data):
             print("Plurals - Line {0}".format(k))
             for l, word in enumerate(line):
                 if word in candidates and word[:-1] in singulars:
-                    dataset[k][l] = word[:-1]
+                    data[k][l] = word[:-1]
 
     if singulars == True:
-        for k,line in enumerate(dataset):
+        for k,line in enumerate(data):
             print("Plurals inflect - Line {0}".format(k))
             for l, word in enumerate(line):
                 if p.singular_noun(word) == False:
                     continue
                 else:
-                    dataset[k][l] = p.singular_noun(word)
+                    data[k][l] = p.singular_noun(word)
 
     if ing_ed == True:
         candidates = set()
         not_inged = set()
-        for line in dataset:
+        for line in data:
             for word in line:
                 if len(word) > 4 and (word.endswith('ed') or word.endswith('ing')):
                     candidates.add(word)
                 elif len(word) > 2:
                     not_inged.add(word)
 
-        for k, line in enumerate(dataset):
+        for k, line in enumerate(data):
             print("inged - line {0}".format(k))
             for l, word in enumerate(line):
                 if word in candidates and word.endswith('ed'):
                     if word[:-2] in not_inged:
-                        dataset[k][l] = word[:-2]
+                        data[k][l] = word[:-2]
                     elif word[:-1] in not_inged:
-                        dataset[k][l] = word[:-1]
+                        data[k][l] = word[:-1]
                 elif word in candidates and word.endswith('ing'):
                     if word[:-3] in not_inged:
-                        dataset[k][l] = word[:-3]
+                        data[k][l] = word[:-3]
 
-    # This was needed to make the dataset compatible with the FeatureUnion format
-    dataset = [' '.join(x) for x in dataset]
+    # This was needed to make the data compatible with the FeatureUnion format
+    data = [' '.join(x) for x in data]
 
-    return dataset
+    return data
 
 # Read training data
 train_ids = list()
@@ -195,15 +188,12 @@ print("Edges: ", G.number_of_edges())
 # (1) out-degree of node
 # (2) in-degree of node
 # (3) average degree of neighborhood of node
-bc_k = 10240
 avg_neig_deg = nx.average_neighbor_degree(G, nodes=train_ids)
-#bw_centrality = nx.betweenness_centrality(G)#,k=bc_k)# normalized=False)
 train_graph_properties = list()
 for i in range(len(train_ids)):
     train_outdeg = G.out_degree(train_ids[i])
     train_indeg = G.in_degree(train_ids[i])
     train_avg_neighbour_deg = avg_neig_deg[train_ids[i]]
-    #train_bw_centr = bw_centrality[train_ids[i]]
     train_graph_properties.append([train_outdeg,train_indeg,train_avg_neighbour_deg])#,train_bw_centr])
 
 train_abstracts = list()
@@ -211,14 +201,12 @@ train_titles    = list()
 train_authors   = list()
 train_citations_in = list()
 train_citations_out = list()
-train_years = list()
 for i in train_ids:
     train_abstracts.append(df.loc[df['id'] == int(i)]['abstract'].iloc[0])
     train_titles.append(df.loc[df['id'] == int(i)]['title'].iloc[0])
     train_authors.append(df.loc[df['id'] == int(i)]['authors'].iloc[0])
     train_citations_in.append(' '.join([str(x) for x in list(df2.loc[df2['paper_id'] == int(i)]['cites'].values)]))
     train_citations_out.append(' '.join([str(x) for x in list(df2.loc[df2['cites'] == int(i)]['paper_id'].values)]))
-    train_years.append(df.loc[df['id'] == int(i)]['year'].iloc[0])
 
 ALLabstracts = transform_data(train_abstracts,stopwords,punctuation,rm_sw=True,rm_smw=True,rm_dg=True,mdash=True)
 ALLtitles    = transform_data(train_titles,stopwords,punctuation,rm_sw=True,rm_smw=True,rm_dg=True,mdash=True)
@@ -234,13 +222,11 @@ with open('test.csv', 'r') as f:
 
 # Create the test matrix. Use the same 3 features as above
 avg_neig_deg = nx.average_neighbor_degree(G, nodes=test_ids)
-#bw_centrality = nx.betweenness_centrality(G)#,k=bc_k)# normalized=False)
 test_graph_properties = list()
 for i in range(len(test_ids)):
     test_outdeg = G.out_degree(test_ids[i])
     test_indeg = G.in_degree(test_ids[i])
     test_avg_neighbour_deg = avg_neig_deg[test_ids[i]]
-    #test_bw_centr = bw_centrality[test_ids[i]]
     test_graph_properties.append([test_outdeg,test_indeg,test_avg_neighbour_deg])#,test_bw_centr])
 
 # Extract the abstract of each test article from the dataframe
@@ -249,14 +235,12 @@ test_titles = list()
 test_authors = list()
 test_citations_in = list()
 test_citations_out = list()
-test_years = list()
 for i in test_ids:
     test_abstracts.append(df.loc[df['id'] == int(i)]['abstract'].iloc[0])
     test_titles.append(df.loc[df['id'] == int(i)]['title'].iloc[0])
     test_authors.append(df.loc[df['id'] == int(i)]['authors'].iloc[0])
     test_citations_in.append(' '.join([str(x) for x in list(df2.loc[df2['paper_id'] == int(i)]['cites'].values)]))
     test_citations_out.append(' '.join([str(x) for x in list(df2.loc[df2['cites'] == int(i)]['paper_id'].values)]))
-    test_years.append(df.loc[df['id'] == int(i)]['year'].iloc[0])
 
 TESTabstracts = transform_data(test_abstracts,stopwords,punctuation,rm_sw=True,rm_smw=True,rm_dg=True,mdash=True)
 TESTtitles    = transform_data(test_titles,stopwords,punctuation,rm_sw=True,rm_smw=True,rm_dg=True,mdash=True)
@@ -269,7 +253,6 @@ write_to_file(ALLtitles,"datasets/train/titles.csv")
 write_to_file(ALLauthors,"datasets/train/authors.csv")
 write_to_file(train_citations_in,"datasets/train/incoming_citations.csv")
 write_to_file(train_citations_out,"datasets/train/outgoing_citations.csv")
-write_to_file(train_years, "datasets/train/years.csv", numbers=True)
 # Use the csv writer for the graph properties
 with open("datasets/train/graph_properties.csv", "w") as f:
     writer = csv.writer(f)
@@ -280,7 +263,6 @@ write_to_file(TESTtitles,"datasets/test/titles.csv")
 write_to_file(TESTauthors,"datasets/test/authors.csv")
 write_to_file(test_citations_in,"datasets/test/incoming_citations.csv")
 write_to_file(test_citations_out,"datasets/test/outgoing_citations.csv")
-write_to_file(test_years, "datasets/test/years.csv", numbers=True)
 # Use the csv writer for the graph properties
 with open("datasets/test/graph_properties.csv", "w") as f:
     writer = csv.writer(f)
--- text_classifier.py
+++ text_classifier.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import numpy as np
-import re
-import string
 import csv
 import logging
 logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
 
-from gensim.parsing.preprocessing import STOPWORDS
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_selection import SelectFromModel
@@ -18,67 +14,44 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline, FeatureUnion, make_union
 from sklearn.metrics import make_scorer, log_loss
-
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import NuSVC, SVC
 from sklearn.tree import DecisionTreeClassifier
-
-import gensim
-from gensim.models import Word2Vec, Doc2Vec
-
-from feature_union_sklearn import ItemSelector, TextStats, AbstractTitleAuthorExtractor, GraphProperties
-
+from feature_union_sklearn import ItemSelector, TextStats, MainExtractor, GraphProperties
 import pdb
-
-def read_corpus(docs, tokens_only=False):
-    for i, line in enumerate(docs):
-        if tokens_only:
-            yield gensim.utils.simple_preprocess(line)
-        else:
-            # For training data, add tags
-            yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-def get_csv(filename):
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        result = list(reader)
-    return result
-
-train_abs = pd.read_csv("datasets/train/abstracts.csv",header=None)
-train_titles = pd.read_csv("datasets/train/titles.csv",header=None)
-train_authors = pd.read_csv("datasets/train/authors.csv", header=None)
-train_citations_in = pd.read_csv("datasets/train/incoming_citations.csv", header=None)
-train_citations_out = pd.read_csv("datasets/train/outgoing_citations.csv", header=None)
-train_outdeg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[0])
-train_indeg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[1])
-train_avg_neigh_deg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[2])
-train_years = pd.read_csv("datasets/train/years.csv", header=None)
+tr_abs = pd.read_csv("datasets/train/abstracts.csv", header=None)
+tr_titles = pd.read_csv("datasets/train/titles.csv", header=None)
+tr_authors = pd.read_csv("datasets/train/authors.csv", header=None)
+tr_cit_in = pd.read_csv("datasets/train/incoming_citations.csv", header=None)
+tr_cit_out = pd.read_csv("datasets/train/outgoing_citations.csv", header=None)
+tr_outdeg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[0])
+tr_indeg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[1])
+tr_avg_ndeg = pd.read_csv("datasets/train/graph_properties.csv", header=None, usecols=[2])
 
 all_train = np.dstack([
-                        train_abs,
-                        train_titles,
-                        train_authors,
-                        train_citations_in,
-                        train_citations_out,
-                        train_outdeg,
-                        train_indeg,
-                        train_avg_neigh_deg,
-                        train_years
+                        tr_abs,
+                        tr_titles,
+                        tr_authors,
+                        tr_cit_in,
+                        tr_cit_out,
+                        tr_outdeg,
+                        tr_indeg,
+                        tr_avg_ndeg
                       ])
 all_train = np.array([t[0] for t in all_train])
 
-test_abs = pd.read_csv("datasets/test/abstracts.csv",header=None)
-test_titles = pd.read_csv("datasets/test/titles.csv",header=None)
-test_authors = pd.read_csv("datasets/test/authors.csv",header=None)
-test_citations_in = pd.read_csv("datasets/test/incoming_citations.csv", header=None)
-test_citations_out = pd.read_csv("datasets/test/outgoing_citations.csv", header=None)
-test_outdeg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[0])
-test_indeg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[1])
-test_avg_neigh_deg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[2])
-test_years = pd.read_csv("datasets/test/years.csv", header=None)
+te_abs = pd.read_csv("datasets/test/abstracts.csv", header=None)
+te_titles = pd.read_csv("datasets/test/titles.csv", header=None)
+te_authors = pd.read_csv("datasets/test/authors.csv", header=None)
+te_cit_in = pd.read_csv("datasets/test/incoming_citations.csv", header=None)
+te_cit_out = pd.read_csv("datasets/test/outgoing_citations.csv", header=None)
+te_outdeg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[0])
+te_indeg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[1])
+te_avg_ndeg = pd.read_csv("datasets/test/graph_properties.csv", header=None, usecols=[2])
 
 # Get the labels from the train dataset
 y_train = list()
@@ -94,49 +67,22 @@ labels = np.unique(y_train)
 #################
 # FEATURE UNION #
 #################
-# This is painful because transform (feature selection) using estimators has been
-# deprecated in scikit learn. So we are going to go with SelectFromModel
-logr_abs = LogisticRegression(penalty='l2',tol=1e-05)
-logr_tit = LogisticRegression(penalty='l2',tol=1e-05)
-logr_aut = LogisticRegression(penalty='l2',tol=1e-05)
-logr_cit_in = LogisticRegression(penalty='l2',tol=1e-05)
-svc_cit_in = SVC(verbose=False,kernel='linear',probability=True)
-logr_cit_out = LogisticRegression(penalty='l2',tol=1e-05)
-svc_cit_out = SVC(verbose=False,kernel='linear',probability=True)
-logr_outdeg = LogisticRegression(penalty='l2',tol=1e-05)
-logr_indeg = LogisticRegression(penalty='l2',tol=1e-05)
-logr_avg_neigh_deg = LogisticRegression(penalty='l2',tol=1e-05)
-logr_year = LogisticRegression(penalty='l2', tol=1e-05)
-mnb_year = MultinomialNB(alpha=0.1)
-rf_year = RandomForestClassifier()
-logr_gprops = LogisticRegression(penalty='l2', tol=1e-05)
-logr_abs_st = LogisticRegression(penalty='l2',tol=1e-05)
-
+# This is painful because transform (feature selection) using
+# estimators has been deprecated in scikit learn. So we are
+# going to go with SelectFromModel
+logr_abs = LogisticRegression(penalty='l2', tol=1e-05)
+logr_tit = LogisticRegression(penalty='l2', tol=1e-05)
+logr_aut = LogisticRegression(penalty='l2', tol=1e-05)
+logr_cit_in = LogisticRegression(penalty='l2', tol=1e-05)
+logr_cit_out = LogisticRegression(penalty='l2', tol=1e-05)
 
 thres_all = None
 pipeline = Pipeline([
-    ('abstracttitleauthor', AbstractTitleAuthorExtractor()),
+    ('main_extractor', MainExtractor()),
 
     # Use FeatureUnion to combine the features from abstracts, titles and authors
     ('union', FeatureUnion(
         transformer_list=[
-
-            # Pipeline for pulling features from abstracts
-            # ('abstract', Pipeline([
-            #     ('selector', ItemSelector(key='abstract')),
-            #     ('vect', CountVectorizer(decode_error='ignore', stop_words='english', max_df=0.6, min_df=0.001,ngram_range=(1,2))),
-            #     ('tfidf', TfidfTransformer(norm='l2',sublinear_tf=True)),
-            #     ('sfm_abs', SelectFromModel(logr_abs,threshold=thres_all)),
-            # ])),
-
-            # Pipeline for pulling features from titles
-            # ('title', Pipeline([
-            #    ('selector', ItemSelector(key='title')),
-            #    ('vect', CountVectorizer(decode_error='ignore', stop_words='english', max_df=0.18, min_df=0)),
-            #    ('tfidf', TfidfTransformer(norm='l2',sublinear_tf=True)),
-            #    ('sfm_tit', SelectFromModel(logr_tit,threshold=thres_all)),
-            # ])),
-
             ('abstract_title', Pipeline([
                 ('selector', ItemSelector(key='abstract_title')),
                 ('vect', CountVectorizer(decode_error='ignore', stop_words='english', max_df=0.6, min_df=0.001,ngram_range=(1,2))),
@@ -151,14 +97,6 @@ pipeline = Pipeline([
                 ('tfidf', TfidfTransformer(norm='l2',sublinear_tf=True)),
                 ('sfm_aut', SelectFromModel(logr_aut,threshold=0.55)),
             ])),
-
-            #('year', Pipeline([
-            #   ('selector', ItemSelector(key='year')),
-            #   #('vect', CountVectorizer(decode_error='ignore', stop_words='english', max_df=0.03, min_df=0)),
-            #   #('tfidf', TfidfTransformer(norm='l2',sublinear_tf=True)),
-            #   ('vect_year', DictVectorizer()),
-            #   ('sfm_year', SelectFromModel(logr_year,threshold=thres_all)),
-            #])),
 
             # Pipeline for pulling features from authors
             ('incoming_citations', Pipeline([
@@ -187,7 +125,6 @@ pipeline = Pipeline([
                 ('selector', ItemSelector(key='abstract')),
                 ('stats', TextStats()),  # returns a list of dicts
                 ('vect_abs_stats', DictVectorizer()),
-                #('sfm_abs', SelectFromModel(logr_abs_st,threshold=0.2)),
             ])),
 
             # Pipeline for title stats
@@ -196,10 +133,9 @@ pipeline = Pipeline([
                ('stats', TextStats()),  # returns a list of dicts
                ('vect_titles_stats', DictVectorizer()),
             ])),
-    
         ],
 
-        # weight components in FeatureUnion - Check the result notes file for more
+        # weight components in FeatureUnion
         transformer_weights={
             'abstract': 1.65,
             'abstract_title': 1.65,
@@ -210,19 +146,10 @@ pipeline = Pipeline([
             'outgoing_citations': 1.3,
 			#'gprops': 0.6,
             #'aut_stats': 0.7,
-            # 'year': 1.2,
         },
     )),
 
-    # Use Logistic Regression again on the combined features
-    #('sgd', SGDClassifier(loss='modified_huber')),
-    # Default solver is liblinear
     ('logr', LogisticRegression(penalty='l2',tol=0.0001)), # C=1.25 results in a slight improvement
-    # Use an SVC classifier on the combined features - Takes forever
-    #('svc', SVC(verbose=False,kernel='linear',probability=True)),
-    # Random forest?
-    #('rfc', RandomForestClassifier(n_estimators=10,max_features='log2',max_depth=5)),
-    #('clf', MLPClassifier(hidden_layer_sizes=(2,2,2,2),verbose=True)),
 ])
 
 # uncommenting more parameters will give better exploring power but will
@@ -232,37 +159,17 @@ parameters = {
 
 log_loss_scorer = make_scorer(log_loss, greater_is_better=False, needs_proba=True)
 grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=10,scoring=log_loss_scorer)
-
 grid_search.fit(all_train,y_train)
 
-# WORD EMBEDDINGS
-# vocab = [s.split() for s in x_train]
-# vocab_y = [s.split() for s in y_train]
-#embeddings = Word2Vec(vocab,min_count=1)
-#embeddings.wv['george']
-
-# DOC EMBEDDINGS
-# train_corpus = list(read_corpus(x_train))
-# test_corpus = list(read_corpus(x_test,tokens_only=True))
-# doc_embeddings = Doc2Vec(min_count=1)
-# doc_embeddings.build_vocab(train_corpus)
-# doc_embeddings.train(train_corpus,total_examples=doc_embeddings.corpus_count,epochs=55)
-# doc_embeddings.infer_vector(['physics','good'])
-
-# x_train_emb = [doc_embeddings.infer_vector(x) for x in vocab]
-# y_train_emb = [doc_embeddings.infer_vector(y) for y in vocab_y]
-
-#x_train = np.dstack([tr_abs,tr_tit,tr_aut])
 x_test = np.dstack([
-                        test_abs,
-                        test_titles,
-                        test_authors,
-                        test_citations_in,
-                        test_citations_out,
-                        test_outdeg,
-                        test_indeg,
-                        test_avg_neigh_deg,
-                        test_years
+                        te_abs,
+                        te_titles,
+                        te_authors,
+                        te_cit_in,
+                        te_cit_out,
+                        te_outdeg,
+                        te_indeg,
+                        te_avg_ndeg
                   ])
 x_test = np.array([t[0] for t in x_test])
 
@@ -272,9 +179,7 @@ best_parameters = grid_search.best_estimator_.get_params()
 for param_name in sorted(parameters.keys()):
     print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
-####################
-# Get test IDs too #
-####################
+# Get test IDs too
 test_ids = list()
 with open('test.csv', 'r') as f:
     next(f)
@@ -287,7 +192,6 @@ y_pred = grid_search.predict_proba(x_test)
 with open('sample_submission.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     lst = grid_search.classes_.tolist()
-    #lst = pipeline.classes_.tolist()
     lst.insert(0, "Article")
     writer.writerow(lst)
     for i,test_id in enumerate(test_ids):
